1cff564c24cc85056f8d10403b5517fe5643cb1b
Harshit Sachdeva <105371858+harshit070104@users.noreply.github.com>
2023-10-01 19:40:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+let slider = tns({
+    container : ".my-slider",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 500,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "6",
+
+    responsive : {
+        1200:{
+            items : 6,
+            // gutter : 20
+        },
+        960:{
+            items : 4,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+
+// by changing myslider->myslider1 and slider -> slider 1
+let slider1 = tns({
+    container : ".my-slider1",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 800,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "4",
+
+    responsive : {
+        1200:{
+            items : 4,
+            // gutter : 20
+        },
+        960:{
+            items : 3,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+                                                                                                
+let slider2 = tns({
+    container : ".my-slider2",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 500,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "6",
+
+    responsive : {
+        1200:{
+            items : 6,
+            // gutter : 20
+        },
+        960:{
+            items : 4,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+
+let slider3 = tns({
+    container : ".my-slider3",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 500,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "6",
+
+    responsive : {
+        1200:{
+            items : 6,
+            // gutter : 20
+        },
+        960:{
+            items : 4,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+
+let slider4 = tns({
+    container : ".my-slider4",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 500,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "4",
+
+    responsive : {
+        1200:{
+            items : 4,
+            // gutter : 20
+        },
+        960:{
+            items : 3,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+
+let slider5 = tns({
+    container : ".my-slider5",
+    "slideBy" : "1",
+    "loop" : false,
+    "mouseDrag" : true,
+    "controls" : true,
+    "arrowKeys" : false,  
+    "nav" : false,
+    "autoplayButtonOutput" : false,
+    "speed" : 800,
+        
+    "controlsText" : ["<p>&#11164;</p>","<p>&#11166;</p>"],        
+    "items" : "4",
+
+    responsive : {
+        1200:{
+            items : 4,
+            // gutter : 20
+        },
+        960:{
+            items : 3,
+            // gutter : 20
+        },
+        700:{
+            items:3,
+            // gutter:20
+        },
+        500:{
+            items:2,
+            // gutter : 20
+        },        
+        320:{
+            items:1,
+            // gutter:20
+        }
+    }
+})
+
+        
